ffbaa24eeb063bebbf23454cae242b01b97bb6a7
tomasort <tomasvortegar@gmail.com>
2022-05-14 19:57:16 -0400

    mistake

--- submit-jobs.sh
+++ submit-jobs.sh
@@ -1,3 +1,3 @@
 sbatch scripts/train-efficientNet-double-frame.sbatch
-sbatch scripts/train-resnet-double-frame copy.sbatch
+sbatch scripts/train-resnet-double-frame.sbatch
 sbatch scripts/train-resnet.sbatch
